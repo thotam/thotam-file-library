aeb8e4be7e8ef0def2e2721f3826177cfbb1e3c9
thanhtamtotaa <thanhtamtqno1@gmail.com>
2021-07-20 13:25:52 +0700

    Update FilePondUploadTraits.php

--- src/Traits/FilePondUploadTraits.php
+++ src/Traits/FilePondUploadTraits.php
@@ -24,17 +24,21 @@ trait FilePondUploadTraits
 
         $tmpPath = FilePondUpload::whereIn('id', $serverID)->get()->pluck('livewire_patch')->toArray();
 
-        if ($isMultiple) {
-            $file = collect($tmpPath)->map(function ($i) {
-                return TemporaryUploadedFile::createFromLivewire($i);
-            })->toArray();
-        } else {
-            $file = TemporaryUploadedFile::createFromLivewire($tmpPath[0]);
-            // If the property is an array, but the upload ISNT set to "multiple"
-            // then APPEND the upload to the array, rather than replacing it.
-            if (is_array($value = $this->getPropertyValue($name))) {
-                $file = array_merge($value, [$file]);
+        if (!!$tmpPath) {
+            if ($isMultiple) {
+                $file = collect($tmpPath)->map(function ($i) {
+                    return TemporaryUploadedFile::createFromLivewire($i);
+                })->toArray();
+            } else {
+                $file = TemporaryUploadedFile::createFromLivewire($tmpPath[0]);
+                // If the property is an array, but the upload ISNT set to "multiple"
+                // then APPEND the upload to the array, rather than replacing it.
+                if (is_array($value = $this->getPropertyValue($name))) {
+                    $file = array_merge($value, [$file]);
+                }
             }
+        } else {
+            $file = null;
         }
 
         $this->syncInput($name, $file);
